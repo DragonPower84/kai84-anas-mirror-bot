a16d4637fe89de20463446fa47e1c7c43a163b6d
DragonPower84 <patrobighnesh8@gmail.com>
2022-02-06 20:42:56 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -9,8 +9,8 @@ from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
 from bot import dispatcher, LOGGER, CLONE_LIMIT, STOP_DUPLICATE, download_dict, download_dict_lock, Interval
-from bot.helper.ext_utils.bot_utils import get_readable_file_size, is_gdrive_link, is_gdtot_link, new_thread, appdrive_dl
-from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, is_gdrive_link, is_gdtot_link, new_thread
+from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot, appdrive_dl
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
 
 @new_thread
