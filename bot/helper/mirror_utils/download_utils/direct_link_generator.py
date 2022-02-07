d63f46ce0c2cac8f8a91fef92187a878fb358c98
DragonPower84 <patrobighnesh8@gmail.com>
2022-02-07 17:56:18 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -11,6 +11,7 @@ for original authorship. """
 import requests
 import re
 
+from lxml import etree
 from base64 import b64decode
 from urllib.parse import urlparse, unquote
 from json import loads as jsnloads
